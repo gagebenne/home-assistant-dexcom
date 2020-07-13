0a46a8987962f04bf4abfeef7e218180a3ce3b43
Gage Benne <gagedbenne@gmail.com>
2020-07-13 17:01:08 -0400

    Sunset repository in favor of official integration

--- dexcom/sensor.py
+++ dexcom/sensor.py
@@ -1,15 +1,15 @@
 """Support for Dexcom sensors."""
-from homeassistant.const import CONF_UNIT_OF_MEASUREMENT, CONF_USERNAME, STATE_UNKNOWN
+from homeassistant.const import CONF_UNIT_OF_MEASUREMENT, CONF_USERNAME
 from homeassistant.helpers.entity import Entity
 
-from .const import DOMAIN, GLUCOSE_TREND_ICON, GLUCOSE_VALUE_ICON, MG_DL
+from .const import COORDINATOR, DOMAIN, GLUCOSE_TREND_ICON, GLUCOSE_VALUE_ICON, MG_DL
 
 
 async def async_setup_entry(hass, config_entry, async_add_entities):
     """Set up the Dexcom sensors."""
-    coordinator = hass.data[DOMAIN][config_entry.entry_id]
+    coordinator = hass.data[DOMAIN][config_entry.entry_id][COORDINATOR]
     username = config_entry.data[CONF_USERNAME]
-    unit_of_measurement = config_entry.data[CONF_UNIT_OF_MEASUREMENT]
+    unit_of_measurement = config_entry.options[CONF_UNIT_OF_MEASUREMENT]
     sensors = []
     sensors.append(DexcomGlucoseTrendSensor(coordinator, username))
     sensors.append(DexcomGlucoseValueSensor(coordinator, username, unit_of_measurement))
@@ -48,16 +48,14 @@ class DexcomGlucoseValueSensor(Entity):
     @property
     def state(self):
         """Return the state of the sensor."""
-        return (
-            getattr(self._coordinator.data, self._attribute_unit_of_measurement)
-            if self._coordinator.data
-            else STATE_UNKNOWN
-        )
+        if self._coordinator.data:
+            return getattr(self._coordinator.data, self._attribute_unit_of_measurement)
+        return None
 
     @property
     def available(self):
         """Return True if entity is available."""
-        return bool(self._coordinator.data)
+        return self._coordinator.last_update_success
 
     @property
     def should_poll(self):
@@ -75,11 +73,9 @@ class DexcomGlucoseValueSensor(Entity):
 
     async def async_added_to_hass(self):
         """When entity is added to hass."""
-        self._coordinator.async_add_listener(self.async_write_ha_state)
-
-    async def async_will_remove_from_hass(self):
-        """When entity will be removed from hass."""
-        self._coordinator.async_remove_listener(self.async_write_ha_state)
+        self.async_on_remove(
+            self._coordinator.async_add_listener(self.async_write_ha_state)
+        )
 
 
 class DexcomGlucoseTrendSensor(Entity):
@@ -100,25 +96,21 @@ class DexcomGlucoseTrendSensor(Entity):
     @property
     def icon(self):
         """Return the icon for the frontend."""
-        return (
-            GLUCOSE_TREND_ICON[self._coordinator.data.trend]
-            if self._coordinator.data
-            else GLUCOSE_TREND_ICON[0]
-        )
+        if self._coordinator.data:
+            return GLUCOSE_TREND_ICON[self._coordinator.data.trend]
+        return GLUCOSE_TREND_ICON[0]
 
     @property
     def state(self):
         """Return the state of the sensor."""
-        return (
-            self._coordinator.data.trend_description
-            if self._coordinator.data
-            else STATE_UNKNOWN
-        )
+        if self._coordinator.data:
+            return self._coordinator.data.trend_description
+        return None
 
     @property
     def available(self):
         """Return True if entity is available."""
-        return bool(self._coordinator.data)
+        return self._coordinator.last_update_success
 
     @property
     def should_poll(self):
@@ -136,8 +128,6 @@ class DexcomGlucoseTrendSensor(Entity):
 
     async def async_added_to_hass(self):
         """When entity is added to hass."""
-        self._coordinator.async_add_listener(self.async_write_ha_state)
-
-    async def async_will_remove_from_hass(self):
-        """When entity will be removed from hass."""
-        self._coordinator.async_remove_listener(self.async_write_ha_state)
+        self.async_on_remove(
+            self._coordinator.async_add_listener(self.async_write_ha_state)
+        )
